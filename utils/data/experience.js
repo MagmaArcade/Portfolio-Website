30905c7b954489f8b67e3d0cb856d75f0257877f
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2024-05-27 00:09:24 +1000

    p

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
+    title: 'Retail Associate',
+    company: "Optus Telecommunications",
     duration: "(Jan 2022 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Junior Search Engine Optimization Specialist",
+    company: "IT BOOST Australia",
+    duration: "(2019 - 2020)"
   },
   {
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    duration: "(2018 - Present)"
   }
 ]
