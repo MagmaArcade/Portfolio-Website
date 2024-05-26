c0795c65ecebbf6fdf7f93e4ae6e4195a3bb641f
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2024-05-27 00:22:54 +1000

    s

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,8 @@
-export const experiences = [
+export const educations = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
-  },
-  {
-    id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Bachelor Degree",
+    duration: "2022 - Present",
+    institution: "Swinburne University of Technology",
   }
 ]
