16a1a639640b192585e5a132a0cdfcdbb5b10706
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2024-05-27 00:46:10 +1000

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'Nathan.Hoorbakht@gmail.com',
-    phone: '0449 860 354',
+    phone: '0449860354',
     address: 'Melbourne, Victoria',
     github: 'https://github.com/MagmaArcade',
     facebook: '',
