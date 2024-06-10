f1a80ce4b0106f49bf75cdf396139615b9085f9e
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2024-06-10 15:17:59 +1000

    Update contact-with-captcha.jsx

--- app/components/homepage/contact/contact-with-captcha.jsx
+++ app/components/homepage/contact/contact-with-captcha.jsx
@@ -3,7 +3,7 @@
 import { isValidEmail } from '@/utils/check-email';
 import emailjs from '@emailjs/browser';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { TbMailForward } from "react-icons/tb";
 import { toast } from 'react-toastify';
@@ -20,6 +20,8 @@ function ContactWithCaptcha() {
     required: false,
   });
 
+  const recaptchaRef = useRef();
+
   const checkRequired = () => {
     if (input.email && input.message && input.name) {
       setError({ ...error, required: false });
@@ -27,30 +29,29 @@ function ContactWithCaptcha() {
   };
 
   const handleSendMail = async (e) => {
-    if (!captcha) {
-      toast.error('Please complete the captcha!');
-      return;
-    } else {
-      const res = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/google`, {
-        token: captcha
-      });
-
-      setCaptcha(null);
-      if (!res.data.success) {
-        toast.error('Captcha verification failed!');
-        return;
-      };
-    };
-
     e.preventDefault();
+    
     if (!input.email || !input.message || !input.name) {
       setError({ ...error, required: true });
       return;
     } else if (error.email) {
       return;
-    } else {
-      setError({ ...error, required: false });
-    };
+    }
+
+    const token = await recaptchaRef.current.executeAsync();
+    setCaptcha(token);
+
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/google`, {
+      token: token
+    });
+
+    setCaptcha(null);
+    recaptchaRef.current.reset();
+
+    if (!res.data.success) {
+      toast.error('Captcha verification failed!');
+      return;
+    }
 
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
     const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
@@ -66,10 +67,10 @@ function ContactWithCaptcha() {
           email: '',
           message: '',
         });
-      };
+      }
     } catch (error) {
       toast.error(error?.text || error);
-    };
+    }
   };
 
   return (
@@ -89,7 +90,7 @@ function ContactWithCaptcha() {
         <p className="text-sm text-[#d3d8e8]">
           {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
         </p>
-        <div className="mt-6 flex flex-col gap-4">
+        <form onSubmit={handleSendMail} className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <label className="text-base">Your Name: </label>
             <input
@@ -137,7 +138,8 @@ function ContactWithCaptcha() {
           </div>
           <ReCAPTCHA
             sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-            onChange={(code) => setCaptcha(code)}
+            size="invisible"
+            ref={recaptchaRef}
           />
           <div className="flex flex-col items-center gap-2">
             {error.required &&
@@ -148,16 +150,16 @@ function ContactWithCaptcha() {
             <button
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
-              onClick={handleSendMail}
+              type="submit"
             >
               <span>Send Message</span>
               <TbMailForward className="mt-1" size={18} />
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
-};
+}
 
 export default ContactWithCaptcha;
