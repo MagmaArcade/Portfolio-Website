0286b232ab7de73bb7ac829574e85ddda594a950
Nathan <102445046+MagmaArcade@users.noreply.github.com>
2024-11-04 18:55:08 +1100

    Update index.jsx

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,6 +1,7 @@
 import { projectsData } from '@/utils/data/projects-data';
 import Image from "next/image";
 import GlowCard from "../../helper/glow-card";
+import React from 'react';  // Import React
 
 const Projects = () => {
   return (
@@ -27,23 +28,21 @@ const Projects = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
           {projectsData.slice(0, 4).map((project, index) => (
             <GlowCard key={index} identifier={`project-${index + 1}`}>
-              <div className="from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-full">
-                <div className="p-4 relative">
-                  <Image
-                    src="/blur-23.svg"
-                    alt="Background Blur"
-                    width={1080}
-                    height={200}
-                    className="absolute bottom-0 opacity-80"
-                  />
-                  <div className="flex justify-center mb-4">
-                    <p className="text-xs sm:text-sm text-[#16f2b3]">
-                      {project.duration}
-                    </p>
-                  </div>
-                  <p className="text-center text-[#16f2b3] text-base lg:text-xl">
-                    {project.name}
+              <div className="p-3 relative">
+                <Image
+                  src="/blur-23.svg"
+                  alt="Background Blur"
+                  width={1080}
+                  height={200}
+                  className="absolute bottom-0 opacity-80"
+                />
+                <div className="flex justify-center mb-4">
+                  <p className="text-xs sm:text-sm text-[#16f2b3]">
+                    {project.duration}
                   </p>
+                </div>
+                <div className="text-center text-[#16f2b3] text-base lg:text-xl">
+                  {project.name}
                 </div>
                 <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
                   <code className="font-mono text-xs md:text-sm lg:text-base">
@@ -55,32 +54,38 @@ const Projects = () => {
                     </div>
                     <div>
                       <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-                      <span className="text-gray-400">'</span>
+                      <span className="text-gray-400">&apos;</span>
                       <span className="text-amber-300">{project.name}</span>
-                      <span className="text-gray-400">',</span>
+                      <span className="text-gray-400">&apos;,</span>
                     </div>
+
                     <div className="ml-4 lg:ml-8 mr-2">
-                      <span className="text-white">tools:</span>
-                      <span className="text-gray-400">['</span>
-                      {project.tools.map((tag, i) => (
-                        <React.Fragment key={i}>
-                          <span className="text-amber-300">{tag}</span>
-                          {project.tools.length - 1 !== i && <span className="text-gray-400">', '</span>}
-                        </React.Fragment>
-                      ))}
-                      <span className="text-gray-400">'],</span>
+                      <span className=" text-white">tools:</span>
+                      <span className="text-gray-400">[&apos;</span>
+                      {
+                        project.tools.map((tag, i) => (
+                          <React.Fragment key={i}>
+                            <span className="text-amber-300">{tag}</span>
+                            {
+                              project.tools.length - 1 !== i &&
+                              <span className="text-gray-400">&apos;, &apos;</span>
+                            }
+                          </React.Fragment>
+                        ))
+                      }
+                      <span className="text-gray-400">{"],"}</span>
                     </div>
                     <div>
-                      <span className="ml-4 lg:ml-8 mr-2 text-white">role:</span>
+                      <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
                       <span className="text-orange-400">{project.role}</span>
                       <span className="text-gray-400">,</span>
                     </div>
                     <div className="ml-4 lg:ml-8 mr-2">
-                      <span className="text-white">description:</span>
+                      <span className="text-white">Description:</span>
                       <span className="text-cyan-400">{' ' + project.description}</span>
                       <span className="text-gray-400">,</span>
                     </div>
-                    <div><span className="text-gray-400">{'};'}</span></div>
+                    <div><span className="text-gray-400">{`};`}</span></div>
                   </code>
                 </div>
               </div>
