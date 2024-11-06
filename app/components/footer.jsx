ac51a2ac6fe8eb0b4bbd96cd6ed4c7308c951cea
Nathan <102445046+MagmaArcade@users.noreply.github.com>
2024-11-07 00:07:42 +1100

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -4,14 +4,14 @@ import { IoStar } from "react-icons/io5";
 
 function Footer() {
   return (
-    <div className="relative border-t bg-[A1AEFF] border-[#27293C] text-dark">
+    <div className="relative border-t bg-[#1A1443] border-[#27293C] text-white">
       <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-6 lg:py-10">
         <div className="flex justify-center -z-40">
           <div className="absolute top-0 h-[1px] w-1/2  bg-gradient-to-r from-transparent via-violet-500 to-transparent"></div>
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/N-Hoorbakht/" className="text-[#4453A1]">Nathan Hoorbakht</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/N-Hoorbakht/" className="text-[#A1AEFF]">Nathan Hoorbakht</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
