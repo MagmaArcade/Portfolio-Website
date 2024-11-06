164d4e0846556c6c10c67f39f275d8861ffbfaf2
Nathan <102445046+MagmaArcade@users.noreply.github.com>
2024-11-06 23:44:53 +1100

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -46,7 +46,7 @@ function HeroSection() {
 
           <div className="flex items-center gap-3">
             <Link href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
-              <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#1A1443] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#B5BBEA] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
+              <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0D1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#B5BBEA] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
                 <span>Contact me</span>
                 <RiContactsFill size={16} />
               </button>
